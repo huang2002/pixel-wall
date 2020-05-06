0b43884edfb2f8c603a4ed7ec78464f3aedd0682
hhh <1340545944@qq.com>
2020-05-06 15:53:34 +0800

    init settings

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,34 @@
 // @ts-check
 import { h } from './common.js';
 
+/**
+ * @param {string} text
+ * @param {string} target
+ */
+export const SettingsLabel = (text, target) => (
+    h('label', {
+        class: 'settings-label',
+        for: target,
+    }, [
+        text
+    ])
+);
+
+/**
+ * @param {string} text
+ * @param {() => void} onClick
+ */
+const SettingsButton = (text, onClick) => (
+    h('button', {
+        class: 'settings-button',
+        listeners: {
+            click: onClick,
+        },
+    }, [
+        text
+    ])
+);
+
 export const settingsContainer = (
     h('div', {
         id: 'settings-container',
@@ -9,7 +37,9 @@ export const settingsContainer = (
         h('div', {
             id: 'settings-window',
         }, [
-
-        ])
+            SettingsButton('Back', () => {
+                settingsContainer.style.display = 'none';
+            }),
+        ]),
     ])
 );
