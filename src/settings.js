b8a75ce7cdc7150f72d01f876ddd6e8e3d012f2d
huang2002 <1340545944@qq.com>
2020-05-08 10:15:38 +0800

    add settings

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,19 @@
 // @ts-check
 import { h } from './common.js';
+import {
+    DEFAULT_WIDTH,
+    DEFAULT_HEIGHT,
+    setWallWidth,
+    setWallHeight,
+    DEFAULT_PRIMARY_COLOR,
+    DEFAULT_SECONDARY_COLOR,
+    setPrimaryColor,
+    setSecondaryColor,
+    togglePixelShape,
+} from './wall.js';
+
+const MAX_WIDTH = 50;
+const MAX_HEIGHT = 50;
 
 /**
  * @param {string} text
@@ -12,6 +26,29 @@ export const SettingsLabel = (text, target) => (
     }, [
         text
     ])
+);
+
+/**
+ * @typedef SettingsInputOptions
+ * @property {string} name
+ * @property {string} type
+ * @property {string} init
+ * @property {(event: InputEvent) => void} onInput
+ */
+
+/**
+ * @param {SettingsInputOptions} options
+ */
+const SettingsInput = options => (
+    h('input', {
+        class: 'settings-input',
+        name: options.name,
+        type: options.type,
+        value: options.init,
+        listeners: {
+            input: onInput,
+        },
+    })
 );
 
 /**
@@ -34,9 +71,74 @@ export const settingsContainer = (
         id: 'settings-container',
         style: 'display: none',
     }, [
-        h('div', {
+        h('form', {
             id: 'settings-window',
+            action: 'javascript:;',
         }, [
+            SettingsLabel('Width:', 'width'),
+            SettingsInput({
+                name: 'width',
+                type: 'number',
+                init: '' + DEFAULT_WIDTH,
+                onInput(event) {
+                    /**
+                     * @type {HTMLInputElement}
+                     */
+                    // @ts-ignore
+                    const { target } = event;
+                    const width = +target.value;
+                    if (width > MAX_WIDTH) {
+                        target.value = '' + MAX_WIDTH;
+                    } else {
+                        setWallWidth(width);
+                    }
+                },
+            }),
+            h('br'),
+            SettingsLabel('Height:', 'height'),
+            SettingsInput({
+                name: 'height',
+                type: 'number',
+                init: '' + DEFAULT_HEIGHT,
+                onInput(event) {
+                    /**
+                     * @type {HTMLInputElement}
+                     */
+                    // @ts-ignore
+                    const { target } = event;
+                    const height = +target.value;
+                    if (height > MAX_HEIGHT) {
+                        target.value = '' + MAX_HEIGHT;
+                    } else {
+                        setWallHeight(height);
+                    }
+                },
+            }),
+            h('br'),
+            SettingsLabel('Primary Color:', 'primary-color'),
+            SettingsInput({
+                name: 'primary-color',
+                type: 'color',
+                init: DEFAULT_PRIMARY_COLOR,
+                onInput(event) {
+                    // @ts-ignore
+                    setPrimaryColor(event.target.value);
+                },
+            }),
+            h('br'),
+            SettingsLabel('Secondary Color:', 'secondary-color'),
+            SettingsInput({
+                name: 'secondary-color',
+                type: 'color',
+                init: DEFAULT_SECONDARY_COLOR,
+                onInput(event) {
+                    // @ts-ignore
+                    setSecondaryColor(event.target.value);
+                },
+            }),
+            h('br'),
+            SettingsButton('Toggle Shape', togglePixelShape),
+            h('br'),
             SettingsButton('Back', () => {
                 settingsContainer.style.display = 'none';
             }),
